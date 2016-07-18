baa5c72db8f081012ee73db1458854588332de4d
Georges Dubus <georges.dubus@compiletoi.net>
2016-07-18 15:47:08 +0200

    Smaller releases by using split-objs
    For that, we have a completely different stack-root for releases.
    There is a long bootstrap, but it is worth it.

--- release-mac.sh
+++ release-mac.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -ex
 
-TAG=$(git describe)
-BIN=$(stack path --local-install-root)/bin/fhue
+STACK_ROOT=~/.stack-split
+STACK_WORK=.stack-split
+DIST_DIR=dist
+BIN=$DIST_DIR/fhue
 
-stack build --force-dirty
+
+TAG=$(git describe)
+mkdir -p dist
+
+stack --install-ghc --stack-root $STACK_ROOT --work-dir $STACK_WORK --local-bin-path $DIST_DIR install --force-dirty --split-objs
 upx -9 $BIN
 
 github-release upload -u madjar -r fhue -t $TAG -n fhue-mac -f $BIN
