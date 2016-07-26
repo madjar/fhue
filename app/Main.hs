0ba70ae21b1baf913ae9318948e602a5d87bd935
Georges Dubus <georges.dubus@compiletoi.net>
2016-07-26 15:52:25 +0200

    Move the password functions to System.Keychain

--- app/Main.hs
+++ app/Main.hs
@@ -2,22 +2,15 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 
-import           Control.Monad
 import           Control.Monad.IO.Class
-import           Data.List.Extra            (dropEnd)
 import           Options.Applicative.Simple hiding ((<>))
 import           Paths_fhue                 as Meta
-import           System.Console.Haskeline
 import           System.Console.Regions     (displayConsoleRegions)
-import           System.Exit                (ExitCode (..))
-import           System.Process             (readProcess,
-                                             readProcessWithExitCode)
 import           Text.PrettyPrint.Boxes
-import           Text.Regex.TDFA
 
-import           FHue.FakeHdfs
+import           System.Keychain
+
 import           FHue.Hue
-import           FHue.Shell
 import           FHue.Types
 
 printItems :: [Item] -> IO ()
@@ -73,30 +66,6 @@ runMyHue hueAction = do
     Nothing -> askAndSetLogin hue
   displayConsoleRegions $ runHue hue login password hueAction
     --`catch` \case LoginFailed -> askAndSetLogin hue >> runMyHue hueAction
-
-getLogin :: String -> IO (Maybe (String, String))
-getLogin root = do
-  (exitCode, out, err) <- readProcessWithExitCode "security" ["find-generic-password", "-g", "-s", root] ""
-  let loginRegex = "\"acct\"<blob>=\"(.*)\""
-      passwordRegex = "password: \"(.*)\""
-      -- TODO this is very ugly
-      [[_, login]] = out =~ loginRegex
-      [[_, password]] = err =~ passwordRegex
-  if exitCode == ExitSuccess
-    then return (Just (login, password))
-    else return Nothing
-
-setLogin :: String -> String -> String -> IO ()
-setLogin root login password = do
-  void $ readProcessWithExitCode "security" ["add-generic-password", "-s", root] ""
-  void $ readProcess "security" ["add-generic-password", "-s", root, "-a", login, "-w", password] ""
-
-askAndSetLogin :: String -> IO (String, String)
-askAndSetLogin hue = runInputT defaultSettings $ do
-  Just login <- getInputLine "login: "
-  Just password <- getPassword (Just '*') "pass: "
-  liftIO (setLogin hue login password)
-  return (login, password)
 
 listAction :: String -> Hue ()
 listAction path = do items <- list path
---
+++ src/System/Keychain.hs
@@ -0,0 +1,34 @@
+module System.Keychain (getLogin, setLogin, askAndSetLogin) where
+
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           System.Console.Haskeline
+import           System.Exit                (ExitCode (..))
+import           System.Process             (readProcess,
+                                             readProcessWithExitCode)
+import           Text.Regex.TDFA
+
+
+getLogin :: String -> IO (Maybe (String, String))
+getLogin root = do
+  (exitCode, out, err) <- readProcessWithExitCode "security" ["find-generic-password", "-g", "-s", root] ""
+  let loginRegex = "\"acct\"<blob>=\"(.*)\""
+      passwordRegex = "password: \"(.*)\""
+      -- TODO this is very ugly
+      [[_, login]] = out =~ loginRegex
+      [[_, password]] = err =~ passwordRegex
+  if exitCode == ExitSuccess
+    then return (Just (login, password))
+    else return Nothing
+
+setLogin :: String -> String -> String -> IO ()
+setLogin root login password = do
+  void $ readProcessWithExitCode "security" ["add-generic-password", "-s", root] ""
+  void $ readProcess "security" ["add-generic-password", "-s", root, "-a", login, "-w", password] ""
+
+askAndSetLogin :: String -> IO (String, String)
+askAndSetLogin hue = runInputT defaultSettings $ do
+  Just login <- getInputLine "login: "
+  Just password <- getPassword (Just '*') "pass: "
+  liftIO (setLogin hue login password)
+  return (login, password)
