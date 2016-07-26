718a2148a4aa32d5869a3f6bcf25648adc89761c
Georges Dubus <georges.dubus@compiletoi.net>
2016-07-26 16:02:00 +0200

    Add some basic integration tests
    Closes #9.

--- test/test.hs
+++ test/test.hs
@@ -1,17 +1,43 @@
-import           Test.Tasty
-import           Test.Tasty.HUnit
+import           Test.Hspec
 
-import           Shell
--- TODO try out hspec, and choose between pure hspec and hspec tasty
+import           System.Environment (getEnv)
+import           System.FilePath    ((</>))
+import           System.IO.Extra    (withTempDir)
 
---main = defaultMain tests
-main = runFakeHdfs runShell
+import           FHue.Hue
+import           FHue.Types
+import           System.Keychain
 
-tests :: TestTree
-tests = testGroup "Tests"
- [
-   testCase "True is True" $
-     True @?= True
+main :: IO ()
+main = hspec $ do
+  let hue = "https://hue-bigplay.bigdata.intraxa/"
+  Just (login, password) <- runIO $ getLogin hue
+  let runIt = runHue hue login password
 
+  let hdfsHome = "/user" </> login
 
- ]
+  describe "ls" $ do
+    it "can list files in /" $ do
+      dirs <- runIt $ list "/"
+      map itemName dirs `shouldContain` ["user"]
+
+  describe "put" $ do
+    it "can upload a file" $ withTempDir $ \tmpDir -> do
+      let file = tmpDir </> "testFile"
+      writeFile file "hello"
+      runIt $ upload file hdfsHome
+
+      files <- runIt $ list hdfsHome
+      map itemName files `shouldContain` ["testFile"]
+
+  describe "get" $ do
+    it "can download a file" $ withTempDir $ \tmpDir -> do
+      runIt $ downloadToDir (hdfsHome </> "testFile") tmpDir
+      readFile (tmpDir </> "testFile") `shouldReturn` "hello"
+
+  describe "rm" $ do
+    it "can delete a file" $ do
+      runIt $ remove (hdfsHome </> "testFile")
+      files <- runIt $ list hdfsHome
+      map itemName files `shouldNotContain` ["testFile"]
+
